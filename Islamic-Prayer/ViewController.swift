fa6a23bc88c079eae84621d6111a5813e576b32f
saad <saadsherif02@gmail.com>
2020-09-13 13:02:17 +0200

    Initial Commit

---
+++ Islamic-Prayer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Islamic-Prayer
+//
+//  Created by sa3doola on 9/13/20.
+//  Copyright © 2020 saad sherif. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
